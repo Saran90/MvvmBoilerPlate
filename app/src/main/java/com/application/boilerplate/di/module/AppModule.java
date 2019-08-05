6e49653358abee36355b63983e2ec9e9e565b8d5
Saran Mampatta <saran.mampatta@uvionicstech.com>
2019-08-05 16:27:46 +0530

    Live Data flow added for login.

--- app/src/main/java/com/application/boilerplate/di/module/AppModule.java
+++ app/src/main/java/com/application/boilerplate/di/module/AppModule.java
@@ -50,8 +50,12 @@ import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import com.application.boilerplate.R;
+
+import org.simpleframework.xml.convert.AnnotationStrategy;
+import org.simpleframework.xml.core.Persister;
 
 import java.util.concurrent.TimeUnit;
 
@@ -152,12 +156,25 @@ public class AppModule {
                 preferencesHelper.getToken());
     }
 
+//    @Provides
+//    @Singleton
+//    Retrofit provideRetrofit(OkHttpClient okHttpClient){
+//        return new Retrofit.Builder()
+//                .baseUrl(BuildConfig.BASE_URL)
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+//                .client(okHttpClient)
+//                .build();
+//    }
+
     @Provides
     @Singleton
     Retrofit provideRetrofit(OkHttpClient okHttpClient){
         return new Retrofit.Builder()
                 .baseUrl(BuildConfig.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(SimpleXmlConverterFactory.createNonStrict(
+                        new Persister(new AnnotationStrategy() // important part!
+                        )))
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .client(okHttpClient)
                 .build();
